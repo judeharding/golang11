6d2a187874432098f5454775ffa38428794bc7ca
Jude Harding <jharding@genesisdla.com>
2022-05-26 17:27:53 -0400

    finish proj 3

--- go-bookstore/cmd/main/main.go
+++ go-bookstore/cmd/main/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+// GORM = A GOLANG Object-Relational Mapper (ORM) is a code
+// library that automates the transfer of data stored in
+// relational database tables into objects that are more
+// commonly used in application code.
 
-func main() {
-	fmt.Println("Hello from main")
-}
+import (
+	"log"      // logs out any error
+	"net/http" // used for routing
+
+	"github.com/akhil/go-bookstore/pkg/routes" // importing MY/HIS routes
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql" // space at the front for
+)
+
+func main(){
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r)) // creates a server
+} 
