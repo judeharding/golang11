6f8e664989e3d865af393c052f16340a6ee9014f
Jude Harding <jharding@genesisdla.com>
2022-06-20 16:13:10 -0400

    can no longer make slackagebot work -- quitting

--- slack-age-bot/main.go
+++ slack-age-bot/main.go
@@ -53,3 +53,65 @@ func main(){
 		log.Fatal(err)
 	}
 }
+
+
+// ===== notes =====
+
+// package main
+
+// import (
+// 	"context"
+// 	"fmt"
+// 	"log"
+// 	"os"
+// 	"strconv"
+
+// 	"github.com/shomali11/slacker"
+// )
+
+// // passing event to slackbot to print it out
+// func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent){
+// 	for event := range analyticsChannel{
+// 		// prints to the console
+// 		fmt.Println("Command Events")
+// 		fmt.Println(event.Timestamp)
+// 		fmt.Println(event.Command)
+// 		fmt.Println(event.Parameters)
+// 		fmt.Println(event.Event)
+// 		fmt.Println()
+// 	}
+// }
+
+// func main(){
+// 	os.Setenv("SLACK_BOT_TOKEN", "xoxb-3615121649203-3600651918983-sfUPfvbsryiEwKygDzj3ZC2K") // socket token
+// 	os.Setenv("SLACK_APP_TOKEN", "xapp-1-A03JSUEERQ8-3615140230931-30ad9588ed666ae2e5c414889f265a464fdfe17b239736c8823d738fa0e3937b") // bot token
+
+// 	// var bot = slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN" ))
+// 	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+// 	go printCommandEvents(bot.CommandEvents()) // passes stuff to the slack command
+
+// 	bot.Command("my yob is <year>", &slacker.CommandDefinition{
+// 		Description: "yob calculator",
+// 		Example: "my yob is 2020",
+// 		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter){  // slacker is coming from the slacker package
+// 				year := request.Param("year")
+// 		 		yob, err := strconv.Atoi(year) // converts the string year entered by the user into a number for calculation
+// 				 if err != nil{
+// 					 fmt.Println("Error")
+// 				 }
+// 				 age := 2022-yob
+// 				r := fmt.Sprintf("Your age is %d", age)
+// 				response.Reply(r)
+// 		},
+// 	})
+
+// 	// REQUIRED for stop the bot
+// 	ctx, cancel := context.WithCancel(context.Background())
+// 	defer cancel()
+
+// 	err := bot.Listen(ctx)
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
--- slack-age-bot/notes.md
+++ slack-age-bot/notes.md
@@ -1,1 +1,22 @@
 https://www.youtube.com/watch?v=jFfo23yIWac @ 2.43
+
+you need a slack account - https://api.slack.com/
+
+you should put your tokens in a folder that does NOT get pushed up to github
+
+you will need to work with your SOCKET MODE, EVENT SUBSCRIPTIONS and OAUTH & PERMISSIONS
+
+go mod init github.com/judeharding/NAMEOFPROJECT
+add a package = go git "github.com/shomali11/slacker"
+then
+go mod tidy // to clean up any outdated or duplicate packages
+
+your project should have a go.mod and go.sum files which are like npm install package.json(initiatied by the go mod line in terminal)
+create main.go file
+
+when finished programming, run - go build - in terminal
+
+HOW TO RUN
+
+- in terminal go run main.go
+- in slack type, @age-bot my yob is 1990
